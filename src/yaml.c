f2625e02d79e30989b0ce3439d3308e944e6ea77
Thorsten Greiner <thorsten.greiner@posteo.de>
2024-12-22 19:22:02 +0100

    Read in scoring config from yaml file

--- src/yaml.c
+++ src/yaml.c
@@ -1,3 +1,33 @@
+/*
+
+    Amy - a chess playing program
+
+    Copyright (c) 2002-2024, Thorsten Greiner
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+
+*/
 #include <ctype.h>
 #include <limits.h>
 #include <stdbool.h>
@@ -16,7 +46,7 @@ struct TokenizerState {
     bool flow_style;
 };
 
-static void abort_if_allocation_failed(void *x) {
+void abort_if_allocation_failed(void *x) {
     if (!x) {
         perror("Cannot allocate buffer");
         exit(1);
@@ -445,52 +475,6 @@ struct IntArrayLookupResult get_as_int_array(struct Node *node, char *path,
     struct IntArrayLookupResult lookup_result = {.result_code = OK,
                                                  .elements_read = index};
     return lookup_result;
-}
-
-char *read_file(char *file_name) {
-    FILE *fin = fopen(file_name, "r");
-    if (!fin) {
-        perror("Cannot open file");
-        return NULL;
-    }
-
-    const size_t page_size = 1024;
-    size_t buf_size = page_size;
-    size_t total_bytes_read = 0;
-
-    char *buffer = malloc(buf_size);
-    abort_if_allocation_failed(buffer);
-
-    char *ptr = buffer;
-
-    for (;;) {
-        size_t bytes_read = fread(ptr, 1, page_size, fin);
-
-        printf("bytes_read: %zu\n", bytes_read);
-
-        if (bytes_read == 0)
-            break;
-
-        ptr += bytes_read;
-        total_bytes_read += bytes_read;
-
-        if ((total_bytes_read + page_size) >= buf_size) {
-            buf_size *= 2;
-            buffer = realloc(buffer, buf_size);
-            abort_if_allocation_failed(buffer);
-        }
-    }
-
-    fclose(fin);
-
-    if ((total_bytes_read + 1) >= buf_size) {
-        buf_size += 1;
-        buffer = realloc(buffer, buf_size);
-        abort_if_allocation_failed(buffer);
-    }
-    *ptr = '\0';
-
-    return buffer;
 }
 
 void test_next_token(char *buffer) {
--- src/yaml.h
+++ src/yaml.h
@@ -1,3 +1,34 @@
+/*
+
+    Amy - a chess playing program
+
+    Copyright (c) 2002-2024, Thorsten Greiner
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 #ifndef YAML_H
 #define YAML_H
 
@@ -62,5 +93,6 @@ struct IntLookupResult get_as_int(struct Node *, char *);
 struct ListLookupResult get_as_list(struct Node *, char *);
 struct IntArrayLookupResult get_as_int_array(struct Node *, char *, int (*)[],
                                              int);
+void abort_if_allocation_failed(void *x);
 
 #endif
